7ad69dfcd2deb0bfcba03a6255f2f030b014bf81
lucasmoreirafm <lucasmoreirafm@gmail.com>
2026-01-19 20:10:16 -0300

    Create treino.js

---
+++ treino.js
@@ -0,0 +1,41 @@
+const treino = {
+  Segunda: [
+    { nome: "Supino reto barra", musculo: "Peito", series: 5, descanso: "02:00" },
+    { nome: "Supino inclinado halteres", musculo: "Peito", series: 4, descanso: "01:30" },
+    { nome: "Crucifixo máquina", musculo: "Peito", series: 3, descanso: "01:00" },
+    { nome: "Tríceps francês com halter", musculo: "Tríceps", series: 4, descanso: "01:30" },
+    { nome: "Tríceps corda", musculo: "Tríceps", series: 3, descanso: "01:00" },
+    { nome: "Paralelas", musculo: "Tríceps", series: 2, descanso: "02:00" },
+  ],
+  Terça: [
+    { nome: "Levantamento terra romeno", musculo: "Posterior", series: 5, descanso: "02:00" },
+    { nome: "Mesa flexora deitado", musculo: "Posterior", series: 4, descanso: "01:30" },
+    { nome: "Mesa flexora unilateral", musculo: "Posterior", series: 3, descanso: "01:00" },
+    { nome: "Hip thrust", musculo: "Glúteo", series: 4, descanso: "01:30" },
+    { nome: "Panturrilha em pé", musculo: "Panturrilha", series: 4, descanso: "01:00" },
+  ],
+  Quarta: [
+    { nome: "Rosca inclinada halteres", musculo: "Bíceps", series: 4, descanso: "01:30" },
+    { nome: "Rosca Scott", musculo: "Bíceps", series: 4, descanso: "01:30" },
+    { nome: "Rosca direta barra", musculo: "Bíceps", series: 3, descanso: "01:30" },
+    { nome: "Rosca martelo cruzada", musculo: "Bíceps", series: 3, descanso: "01:00" },
+    { nome: "Rosca punho barra", musculo: "Antebraço", series: 3, descanso: "01:00" },
+    { nome: "Rosca punho reversa", musculo: "Antebraço", series: 3, descanso: "01:00" },
+  ],
+  Sexta: [
+    { nome: "Barra fixa", musculo: "Costas", series: 4, descanso: "02:00" },
+    { nome: "Remada curvada barra", musculo: "Costas", series: 4, descanso: "02:00" },
+    { nome: "Remada baixa", musculo: "Costas", series: 3, descanso: "01:30" },
+    { nome: "Desenvolvimento militar halteres", musculo: "Ombro", series: 4, descanso: "01:30" },
+    { nome: "Elevação lateral halteres", musculo: "Ombro", series: 4, descanso: "01:00" },
+    { nome: "Crucifixo inverso", musculo: "Ombro", series: 3, descanso: "01:00" },
+  ],
+  Sábado: [
+    { nome: "Supino inclinado máquina", musculo: "Peito", series: 4, descanso: "01:30" },
+    { nome: "Crucifixo inclinado halteres", musculo: "Peito", series: 3, descanso: "01:00" },
+    { nome: "Tríceps pulley barra", musculo: "Tríceps", series: 4, descanso: "01:00" },
+    { nome: "Tríceps francês unilateral", musculo: "Tríceps", series: 3, descanso: "01:00" },
+    { nome: "Stiff halteres", musculo: "Posterior", series: 4, descanso: "01:30" },
+    { nome: "Flexora sentada", musculo: "Posterior", series: 3, descanso: "01:00" },
+  ],
+};
